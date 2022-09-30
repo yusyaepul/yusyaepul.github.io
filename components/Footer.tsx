0513f82102deb3f471156a4f0f8e227618e6139f
yus1129 <yusfreak@gmail.com>
2022-09-30 21:33:32 +0700

    Add Page AboutMe

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -20,6 +20,9 @@ export default function Footer() {
           <Link href="/">
             <a className="text-gray-500 hover:text-gray-600 transition">Home</a>
           </Link>
+          <Link href="/about">
+            <a className="text-gray-500 hover:text-gray-600 transition">About</a>
+          </Link>
         </div>
         <div className="flex flex-col space-y-4">
           <ExternalLink href="https://twitter.com/yusfreak">
