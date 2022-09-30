f6899d762e08ea14eb8fa621e94978e6e967f22f
yus1129 <yusfreak@gmail.com>
2022-09-30 20:13:20 +0700

    Initializing

---
+++ components/BlogPostCard.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link';
+import useSWR from 'swr';
+import cn from 'classnames';
+
+import fetcher from 'lib/fetcher';
+import { Views } from 'lib/types';
+
+export default function BlogPostCard({ title, slug, gradient, desc }) {
+  const { data } = useSWR<Views>(`/api/views/${slug}`, fetcher);
+  const views = data?.total;
+
+  return (
+    <Link href={`/`}>
+      <a
+        className={cn(
+          'transform hover:scale-[1.01] transition-all',
+          'rounded-xl w-full md:w-1/3 bg-gradient-to-r p-1',
+          gradient
+        )}
+      >
+        <div className="flex flex-col justify-between h-full bg-white dark:bg-gray-900 rounded-lg p-4">
+          <div className="flex flex-col md:flex-row justify-between">
+            <h4 className="text-lg md:text-lg font-medium mb-6 sm:mb-10 w-full text-gray-900 dark:text-gray-100 tracking-tight">
+              {title}
+            </h4>
+          </div>
+          <div className="flex items-center text-gray-800 dark:text-gray-200 capsize">
+          <h4 className="text-lg md:text-lg font-medium text-gray-900 dark:text-gray-100 tracking-tight" style={{ margin: 'auto' }}>
+              {desc}
+          </h4>
+          </div>
+        </div>
+      </a>
+    </Link>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,111 @@
+import { Suspense } from 'react';
+import Image from 'next/future/image';
+
+import Container from '../components/Container';
+import BlogPostCard from '../components/BlogPostCard';
+import VideoCard from '../components/VideoCard';
+
+export default function Home() {
+  return (
+    <Suspense fallback={null}>
+      <Container>
+        <div className="flex flex-col justify-center items-start max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
+          <div className="flex flex-col-reverse sm:flex-row items-start">
+            <div className="flex flex-col pr-8">
+              <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-1 text-black dark:text-white">
+                Yusyaepul
+              </h1>
+              <h2 className="text-gray-700 dark:text-gray-200 mb-4">
+                Back-end/ Front-end{' '}
+                <span className="font-semibold">Developer</span> .
+              </h2>
+              <p className="text-gray-600 dark:text-gray-400">
+                Hi, Saya <span className="font-semibold">Yusyaepul</span>, Seorang Programmer dengan lebih 10 tahun
+              </p>
+              <p className="text-gray-600 dark:text-gray-400 mb-4">
+              pengalaman menggunakan PHP dan Javascript untuk mengembangkan, menguji dan mengamati aplikasi perusahaan.
+              </p>
+              <p className="text-gray-600 dark:text-gray-400">
+              Telah membuat dan mengembangkan beberapa aplikasi 
+              </p>
+              <p className="text-gray-600 dark:text-gray-400 mb-16">
+              untuk meningkatkan efisiensi dan data management perusahaan.
+              </p>
+               
+            </div>
+            <div className="w-[80px] sm:w-[176px] relative mb-8 sm:mb-0 mr-auto">
+              <Image
+                alt="Yusyaepul"
+                height={176}
+                width={176}
+                src="/avatar.jpg"
+                sizes="30vw"
+                priority
+                className="rounded-full"
+              />
+            </div>
+          </div>
+
+          <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-6 text-black dark:text-white">
+            Tech Stack
+          </h3>
+          <div className="flex gap-2 flex-col md:flex-row">
+            <BlogPostCard
+              title="HTML5, CSS, Bootstrap, JavaScript, React, JQuery"
+              slug="style-guides-component-libraries-design-systems"
+              gradient="from-[#D8B4FE] to-[#818CF8]"
+              desc="Front End"
+            />
+            <BlogPostCard
+              title="PHP, NodeJS, GoLang, Express, CodeIgniter, Laravel"
+              slug="rust"
+              gradient="from-[#6EE7B7] via-[#3B82F6] to-[#9333EA]"
+              desc="Back End"
+            />
+            <BlogPostCard
+              title="Visual Studio Code, Git, Notion, Slack"
+              slug="react-state-management"
+              gradient="from-[#FDE68A] via-[#FCA5A5] to-[#FECACA]"
+              desc="Tools"
+            />
+          </div>
+
+          <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-16 text-black dark:text-white">
+            Hard Skill
+          </h3>
+          <VideoCard
+            index="01"
+            href="/"
+            length=""
+            title="Full-Stack Developer"
+          />
+          <VideoCard
+            index="02"
+            href="/"
+            length=""
+            title="Back-end Developer"
+          />
+          <VideoCard
+            index="03"
+            href="/"
+            length=""
+            title="Front-end Developer"
+          />
+          <VideoCard
+            index="04"
+            href="/"
+            length=""
+            title="Data Engineer"
+          />
+          <VideoCard
+            index="05"
+            href="/"
+            length=""
+            title="Data Analyst"
+          />
+          <span className="h-16" />
+        </div>
+      </Container>
+    </Suspense>
+  );
+}
