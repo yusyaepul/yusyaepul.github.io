d5dbb29bc96dce2a9614c72a2ded4fee0825fefe
Yusyaepul <53283310+yusyaepul@users.noreply.github.com>
2022-09-30 20:37:38 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPlugins = require('next-compose-plugins');
+const optimizedImages = require('next-optimized-images');
 
-module.exports = nextConfig
+module.exports = withPlugins([
+  [optimizedImages, {
+    /* config for next-optimized-images */
+  }],
+
+  // your other plugins here
+
+]);
